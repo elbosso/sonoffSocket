5ae5e1c7f190406d4ebd58aa7bf724aa7052801b
Jürgen Key <sgjfsf@hotmail.com>
2018-07-21 19:23:12 +0200

    Added autoconfiguration via temporary accesspoint - if this does not happen within 180 seconds - it switches over to waiting for configuration via WPS (https://github.com/tzapu/WiFiManager)

--- sonoffSimple/sonoffSimple.ino
+++ sonoffSimple/sonoffSimple.ino
@@ -1,20 +1,23 @@
 // Very simple example sketch for Sonoff switch as described in article "Bastelfreundlich". This sketch is just for learning purposes.
 // For productive use (with MQTT and advanced features) we recommend SonoffSocket.ino in this repository.
 
-// WPS from https://github.com/rudi48/WiFiTelnetToSerialWPS
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
+//needed for wifimanager
+#include <DNSServer.h>
+//#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
 
 //Your Wifi SSID
 const char* ssid = "your_ssid";
 //Your Wifi Key
 const char* password = "your_key";
 
-ESP8266WebServer server(80);
+ESP8266WebServer* server;
 
 int gpio13Led = 13;
 int gpio12Relay = 12;
@@ -44,7 +47,7 @@ bool startWPSPBC() {
 void doSwitchOn()
 {
     relais = 1;
-    digitalWrite(gpio13Led, HIGH);
+    digitalWrite(gpio13Led, LOW);
     digitalWrite(gpio12Relay, relais);
     delay(1000);
 }
@@ -52,7 +55,7 @@ void doSwitchOn()
 void doSwitchOff()
 {
     relais = 0;
-    digitalWrite(gpio13Led, LOW);
+    digitalWrite(gpio13Led, HIGH);
     digitalWrite(gpio12Relay, relais);
     delay(1000); 
 }
@@ -60,9 +63,9 @@ void doSwitchOff()
 void toggle()
 {
     if(relais == 0){
-      server.sendHeader("Location", String("/ein"), true);
+      server->sendHeader("Location", String("/ein"), true);
     }else{
-      server.sendHeader("Location", String("/aus"), true);
+      server->sendHeader("Location", String("/aus"), true);
     }
 }
 
@@ -75,7 +78,7 @@ void setup(void){
   WiFi.mode(WIFI_OFF);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WiFi.SSID().c_str(),WiFi.psk().c_str()); // reading data from EPROM, (last saved credentials)
-//  WiFi.begin("foobar",WiFi.psk().c_str()); // making sure wps happens
+//  WiFi.begin("foobar",WiFi.psk().c_str()); // making sure access point and if not configured in time (180 sec), wps happen
  
   pinMode(gpio13Led, OUTPUT);
   digitalWrite(gpio13Led, lamp);
@@ -109,35 +112,58 @@ void setup(void){
   if(status == WL_CONNECTED) {
     Serial.printf("\nConnected successful to SSID '%s'\n", WiFi.SSID().c_str());
   } else {
-    // WPS button I/O setup
-    Serial.printf("\nCould not connect to WiFi. state='%d'\n", status);
-    Serial.println("Please press WPS button on your router, until mode is indicated.");
-    Serial.println("next press the ESP module WPS button, router WPS timeout = 2 minutes");
+    WiFi.mode(WIFI_OFF);
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset settings - for testing
+    //wifiManager.resetSettings();
+  
+    //sets timeout until configuration portal gets turned off
+    //useful to make it all retry or go to sleep
+    //in seconds
+    digitalWrite(gpio13Led, LOW);
+    wifiManager.setTimeout(180);
     
-    while(digitalRead(gpio0Switch) == HIGH)  // wait for WPS Button active
+    //fetches ssid and pass and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP_12F647"
+    //and goes into a blocking loop awaiting configuration
+    if(!wifiManager.autoConnect("AutoConnectAP_12F647")) 
     {
-      delay(50);
-      if(lamp == 0){
-         digitalWrite(gpio13Led, 1);
-         lamp = 1;
-       }else{
-         digitalWrite(gpio13Led, 0);
-         lamp = 0;
-      }
-      Serial.print(".");
-      yield(); // do nothing, allow background work (WiFi) in while loops
-    }      
-    Serial.println("WPS button pressed");
-    lamp=0;
-    digitalWrite(gpio13Led, 0);
-    
-    if(!startWPSPBC()) {
-       Serial.println("Failed to connect with WPS :-(");  
-    } else {
-      WiFi.begin(WiFi.SSID().c_str(),WiFi.psk().c_str()); // reading data from EPROM, 
-      while (WiFi.status() == WL_DISCONNECTED) {          // last saved credentials
-        delay(500);
-        Serial.print("."); // show wait for connect to AP
+      WiFi.mode(WIFI_OFF);
+      WiFi.mode(WIFI_STA);
+      digitalWrite(gpio13Led, HIGH);
+      // WPS button I/O setup
+      Serial.printf("\nCould not connect to WiFi. state='%d'\n", status);
+      Serial.println("Please press WPS button on your router, until mode is indicated.");
+      Serial.println("next press the ESP module WPS button, router WPS timeout = 2 minutes");
+      
+      while(digitalRead(gpio0Switch) == HIGH)  // wait for WPS Button active
+      {
+        delay(50);
+        if(lamp == 0){
+           digitalWrite(gpio13Led, 1);
+           lamp = 1;
+         }else{
+           digitalWrite(gpio13Led, 0);
+           lamp = 0;
+        }
+        Serial.print(".");
+        yield(); // do nothing, allow background work (WiFi) in while loops
+      }      
+      Serial.println("WPS button pressed");
+      lamp=0;
+      digitalWrite(gpio13Led, 0);
+      
+      if(!startWPSPBC()) {
+         Serial.println("Failed to connect with WPS :-(");  
+      } else {
+        WiFi.begin(WiFi.SSID().c_str(),WiFi.psk().c_str()); // reading data from EPROM, 
+        while (WiFi.status() == WL_DISCONNECTED) {          // last saved credentials
+          delay(500);
+          Serial.print("."); // show wait for connect to AP
+        }
       }
     }
   } 
@@ -147,23 +173,26 @@ void setup(void){
   Serial.print("IP-Adresse: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", [](){
+  server=new ESP8266WebServer(80);
+
+
+  server->on("/", [](){
     toggle();
-    server.send ( 302, "text/plain", "");  
+    server->send ( 302, "text/plain", "");  
   });
   
-  server.on("/ein", [](){
-    server.send(200, "text/html", "Schaltsteckdose ausschalten<p><a href=\"aus\">AUS</a></p>");
+  server->on("/ein", [](){
+    server->send(200, "text/html", "Schaltsteckdose ausschalten<p><a href=\"aus\">AUS</a></p>");
     doSwitchOn();
   });
   
-  server.on("/aus", [](){
-    server.send(200, "text/html", "Schaltsteckdose einschalten<p><a href=\"ein\">EIN</a></p>");
+  server->on("/aus", [](){
+    server->send(200, "text/html", "Schaltsteckdose einschalten<p><a href=\"ein\">EIN</a></p>");
     doSwitchOff();
   });
-  digitalWrite(gpio13Led, LOW);
+  digitalWrite(gpio13Led, HIGH);
   delay(300);
-  server.begin();
+  server->begin();
   Serial.println("HTTP server started");
   
 }
@@ -179,5 +208,5 @@ if(digitalRead(gpio0Switch) == LOW)
 
   }
   //Webserver 
-  server.handleClient();
+  server->handleClient();
 } 
